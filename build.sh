89dd164e6b2ffa75b439522f4530f7c9aa7a6eff
Santtu Pajukanta <santtu.pajukanta@leonidasoy.fi>
2013-10-17 16:42:13 +0300

    add build script

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+
+RPMBUILD_DIR=${RPMBUILD_DIR:-$HOME/buildrpm}
+NODEJS_VERSION=${NODEJS_VERSION:-v0.10.20}
+NODEJS_SRC=${NODEJS_SRC:-http://nodejs.org/dist/$NODEJS_VERSION/node-$NODEJS_VERSION.tar.gz}
+NODEJSRPM_DIR=$PWD
+
+cd $RPMBUILD_DIR/SOURCES
+wget $NODEJS_SRC
+cp $NODEJSRPM_DIR/*.patch .
+cd -
+
+cd $RPMBUILD_DIR/SPECS
+cp $NODEJSRPM_DIR/*.spec .
+cd -
+
+spectool -g -R $RPMBUILD_DIR/SPECS/nodejs.spec
+rpmbuild --clean -ba $RPMBUILD_DIR/SPECS/nodejs.spec
