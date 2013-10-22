e57c158ff0cda6934730b0e7053284638f30d5b8
Kazuhisa Hara <kazuhisya@gmail.com>
2013-10-22 18:18:36 +0900

    - Determine RPM_BUILD_DIR from current rpmbuild environment - Source can be obtained by spectool

--- build.sh
+++ build.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 set -e
 
-RPMBUILD_DIR=${RPMBUILD_DIR:-$HOME/buildrpm}
-NODEJS_VERSION=${NODEJS_VERSION:-v0.10.20}
-NODEJS_SRC=${NODEJS_SRC:-http://nodejs.org/dist/$NODEJS_VERSION/node-$NODEJS_VERSION.tar.gz}
+RPMBUILD_DIR=`rpm --eval %{_topdir}`
 
 pushd `dirname $0` > /dev/null
 NODEJSRPM_DIR=$(pwd)
 popd > /dev/null
 
 cd $RPMBUILD_DIR/SOURCES
-wget $NODEJS_SRC
 cp $NODEJSRPM_DIR/*.patch .
 cd -
 
